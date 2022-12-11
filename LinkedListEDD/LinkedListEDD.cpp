a427912b038bcbf78747472dfc8786853d0f318d
siriuzz <1106901@est.intec.edu.do>
2022-12-10 23:27:39 -0400

    funcion buscar y eliminardato

--- LinkedListEDD/LinkedListEDD.cpp
+++ LinkedListEDD/LinkedListEDD.cpp
@@ -17,11 +17,71 @@ void Insertar(Nodo*& lista, int n) {
 	nuevo_nodo->dato = n;
 
 	Nodo* aux1 = lista;
-	Nodo* aux2;
+	Nodo* aux2 = NULL;
 
-		
+	while ((aux1 != NULL) && (aux1->dato > n)) {
+		aux2 = aux1;
+		aux1 = aux1->siguiente;
+	}
+
+	if (lista == aux1) {
+		lista = nuevo_nodo;
+	}
+	else {
+		aux2->siguiente = nuevo_nodo;
+	}
+
+	nuevo_nodo->siguiente = aux1;
 }
 
+
+//Buscar elemento en la lista
+void Buscar(Nodo * lista, int n) {
+	bool bandera = false;
+
+	Nodo* actual = new Nodo();
+	actual = lista;
+
+	while ((actual != NULL) && (actual->dato <= n)) {
+		if (actual->dato == n) {
+			bandera = true;
+		}
+		actual = actual->siguiente;
+	}
+
+	if (bandera == true) {
+		cout << "El elemento " << n << " SI fue encontrado en la lista" << endl;
+	}
+	else {
+		cout << "El elemento " << n << " NO fue encontrado en la lista" << endl;
+	}
+}
+
+void EliminarDato(Nodo*& lista, int n) {
+	if (lista != NULL) {
+		Nodo* aux{};
+		Nodo* anterior = NULL;
+
+		while((aux != NULL) && (aux->dato != n)) {
+			anterior = aux;
+			aux = aux->siguiente;
+		}
+
+		if (aux == NULL) {
+			cout << "Elemento no encontrado, intente de nuevo";
+			system("PAUSE");
+			return;
+		}
+		else if (anterior == NULL) {
+			lista = lista->siguiente;
+			delete aux;
+		}
+		else {
+			anterior->siguiente = aux->siguiente;
+			delete aux;
+		}
+	}
+}
 
 //Funcion para validar entrada de un numero
 string ValidarNumero(string str)
